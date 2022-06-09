dcd9df41915c122941ff4450c7ffee98886ee45e
penina wanjiru <penina.wanjiru@student.moringaschool.com>
2022-06-09 13:51:26 +0300

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const companyName = 'Scuber';
+const companyName= "Scuber"
 
-let mostProfitableNeighborhood = 'Chelsea'
+let mostProfitableNeighborhood ="Chelsea"
 
-let companyCeo = 'Susan Smith'
-
+let companyCeo= "Susan Smith"
